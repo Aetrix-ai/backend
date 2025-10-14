02bcb764bd06430736e8f824573d54c43c8ec8f0
ashintv <ashintv2003@gmail.com>
2025-10-14 21:48:59 +0530

    feat lib

---
+++ src/lib/schema.ts
@@ -0,0 +1,21 @@
+import {z } from "zod";
+
+export const userRegisterSchema = z.object({
+    
+});
+
+export const userLoginSchema = z.object({
+
+});
+
+export const projectSchema = z.object({
+
+});
+
+export const eventSchema = z.object({
+
+});
+
+export const updateUserSchema = z.object({
+
+});
