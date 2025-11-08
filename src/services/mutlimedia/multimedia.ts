a9002115e8e1c0525f5a639d76ca8ac97de531dc
aksreelakshmi22 <aksreelakshmi22@gmail.com>
2025-11-08 17:02:38 +0530

    feat multimedia routrt

---
+++ src/services/mutlimedia/multimedia.ts
@@ -0,0 +1,25 @@
+
+
+
+export function uploadImages() {
+  // upload images/videos to s3 bucket and return urls
+  // use multer-s3 or aws-sdk
+  // return array of urls
+}
+
+export function deleteImages() {
+}
+
+export function uploadVideos() {
+}
+
+export function deleteVideos() {
+}
+
+
+export function uploadPdfs() {
+}
+
+export function deletePdfs() {
+}
+
