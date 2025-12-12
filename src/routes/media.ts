07b6a5158c030902df8e07b7e8773986b90da518
ASHIN T V <146656061+ashintv@users.noreply.github.com>
2025-12-12 19:04:29 +0530

    feat: get token endpoint

---
+++ src/routes/media.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import ImageKit from "@imagekit/nodejs";
+import { Config } from "../config";
+export const mediaRouter: Router = Router();
+
+/**
+ * provide media authorization token
+ * for client to upload media to third-party services
+ */
+
+const client = new ImageKit({
+
+  privateKey: Config.IMAGEKIT_PRIVATE_KEY,
+});
+mediaRouter.get("/authorization", (req, res) => {
+  try {
+    const authenticationParameters = client.helper.getAuthenticationParameters();
+    res.json(authenticationParameters);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to get authorization parameters" });
+  }
+});
+
+
+
