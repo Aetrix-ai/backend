688a38f9b09d1f7931b919d69faf5fc4d4c09f66
ASHIN T V <146656061+ashintv@users.noreply.github.com>
2026-01-10 22:22:46 +0530

    log update

--- src/routes/media.ts
+++ src/routes/media.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import ImageKit from "@imagekit/nodejs";
 import { Config } from "../config.js";
+import logger from "../lib/logger.js";
 export const mediaRouter: Router = Router();
 
 /**
@@ -17,6 +18,7 @@ mediaRouter.get("/authenticate-upload", (req, res) => {
     const authenticationParameters = client.helper.getAuthenticationParameters();
     res.json(authenticationParameters);
   } catch (error) {
+    logger.error({error})
     res.status(500).json({ message: "Failed to get authorization parameters" });
   }
 });
