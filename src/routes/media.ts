ce471a4016dc486d3355bc5deb58f6e59477205f
ASHIN T V <146656061+ashintv@users.noreply.github.com>
2026-01-10 22:39:53 +0530

    fix : added custom token for image uploads

--- src/routes/media.ts
+++ src/routes/media.ts
@@ -2,27 +2,35 @@ import { Router } from "express";
 import ImageKit from "@imagekit/nodejs";
 import { Config } from "../config.js";
 import logger from "../lib/logger.js";
+import jwt from "jsonwebtoken";
 export const mediaRouter: Router = Router();
 
 /**
  * provide media authorization token
  * for client to upload media to third-party services
  */
-export const runtime = "nodejs";
+
 const client = new ImageKit({
   privateKey: Config.IMAGEKIT_PRIVATE_KEY,
 });
 
 mediaRouter.get("/authenticate-upload", (req, res) => {
   try {
-    const authenticationParameters = client.helper.getAuthenticationParameters();
+    //@ts-ignore
+    const user = req.user.id;
+    const token = jwt.sign(
+      {
+        user: user,
+        type: "media",
+      },
+      Config.JWT.USER_JWT_SECRET
+    );
+
+    const authenticationParameters = client.helper.getAuthenticationParameters(token);
     res.json(authenticationParameters);
   } catch (error) {
-    console.log(error)
-    logger.error({error})
+    console.log(error);
+    logger.error({ error });
     res.status(500).json({ message: "Failed to get authorization parameters" });
   }
 });
-
-
-
